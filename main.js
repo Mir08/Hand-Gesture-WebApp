031dda2931ffe878425386706bac0099b2376801
Mir08 <78381939+Mir08@users.noreply.github.com>
2021-11-06 10:53:01 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+Webcam.set({
+    width:400,
+    height:400,
+    image_format:"png",
+    png_quality:90
+});
+camera=document.getElementById("camera");
+Webcam.attach("#camera");
+function takesnapshot(){
+Webcam.snap(function(data_uri){
+    document.getElementById("result").innerHTML='<img id="meep" src="'+data_uri+'">';
+})
+}
+console.log("ml5 version",ml5.version);
+classifier = ml5.imageClassifier("https://storage.googleapis.com/tm-model/NMCA1UAun/model.json",modelLoaded);
+function modelLoaded(){
+    console.log("model has been loaded");
+}
+function identify(){
+    img = document.getElementById("Meep");
+    classifier.classify(img,gotResult);
+}
+function gotResult(error,results){
+if(error){
+    console.error(error);
+}
+else{
+    console.log(results);
+    document.getElementById("objects").innerHTML=results[0].label;
+    document.getElementById("Accuracy").innerHTML=results[0].confidence.toFixed(3);
+
+}
+}
